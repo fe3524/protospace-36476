59749a519e06d24b5915823582d84c03685112d9
fe3524 <fe212045@grace.ocn.ne.jp>
2021-09-19 15:50:04 +0900

    コメント機能の修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,15 @@
 class CommentsController < ApplicationController
 
   def create
-    #@prototype = Prototype.find(params[:prototype_id])
     @comment = Comment.new(comment_params)
     if @comment.save
       redirect_to root_path
     else
-      #@prototypes = @comment.prototype_id.includes(:user)
       @prototype = @comment.prototype
       @comments = @prototype.comments
       render "prototypes/show"
     end
   end
-
-  # def show
-  #   @comment = Comment.new
-  #   @comments = @prototype.comments.includes(:user)
-  # end
 
   private
   def comment_params
