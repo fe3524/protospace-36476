aad132620499f3d33c06ac88b7dc183b2f58df41
fe3524 <fe212045@grace.ocn.ne.jp>
2021-09-19 14:20:06 +0900

    コメント機能のリンク修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,21 +6,14 @@ class CommentsController < ApplicationController
     if @comment.save
       redirect_to root_path
     else
-      #@prototypes = @comment.prototype_id.includes(:user)
       @prototype = @comment.prototype
       @comments = @prototype.comments
       render "prototypes/show"
     end
   end
 
-  # def show
-  #   @comment = Comment.new
-  #   @comments = @prototype.comments.includes(:user)
-  # end
-
   private
   def comment_params
     params.require(:comment).permit(:id, :text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
   end
-
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,5 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @prototype = @user.prototypes
   end
+
 end
